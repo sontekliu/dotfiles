132788775a1ca50b81a964bfe900208d428fe752
sontekliu <sontek@yeah.net>
2023-09-29 16:38:08 +0800

    修改nvim配置

--- macOS/config/nvim/lua/core/keymaps.lua
+++ macOS/config/nvim/lua/core/keymaps.lua
@@ -18,13 +18,14 @@ keymap.set("n", "<leader>sh", "<C-w>s") -- 垂直新增窗口
 -- 取消高亮
 keymap.set("n", "<leader><space>", ":nohl<CR>")
 
--- 切换buffer
-keymap.set("n", "<C-L>", ":bnext<CR>")
-keymap.set("n", "<C-H>", ":bprevious<CR>")
 
 -- ---------- 插件 ---------- ---
 -- nvim-tree
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+
+-- 切换buffer
+keymap.set("n", "<C-L>", ":bnext<CR>")
+keymap.set("n", "<C-H>", ":bprevious<CR>")
 
 -- hop cmd
 keymap.set("n", "<leader>hw", ":HopWord<CR>")
